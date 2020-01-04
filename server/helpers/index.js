c288a4109ae5253b3e272bad54a4543c74610b32
arda zaman <ardazamanla@gmail.com>
2020-01-04 14:52:54 +0300

    Puppeteer options removed

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -3,17 +3,8 @@ import puppeteer from 'puppeteer';
 const PUPPETEER_OPTIONS = {
   headless: true,
   args: [
-    '--disable-gpu',
-    '--disable-dev-shm-usage',
     '--disable-setuid-sandbox',
-    '--timeout=30000',
-    '--no-first-run',
     '--no-sandbox',
-    '--no-zygote',
-    '--single-process',
-    "--proxy-server='direct://'",
-    '--proxy-bypass-list=*',
-    '--deterministic-fetch',
   ],
 };
 
