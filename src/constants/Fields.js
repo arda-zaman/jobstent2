84420753013d51b20a96f619e256691839c21eac
arda zaman <ardazamanla@gmail.com>
2020-01-21 18:40:41 +0300

    Field properties improved

--- src/constants/Fields.js
+++ src/constants/Fields.js
@@ -156,7 +156,7 @@ export const special_fields = [
 
 export const field_properties = {
   jbt_text: {
-    'fontFamily': {
+    fontFamily: {
       fieldType: 'dropdown',
       label: 'Font Family',
       additionalClass: 'inline',
@@ -166,7 +166,7 @@ export const field_properties = {
         { value: "Helvetica", label: "Helvetica" },
       ],
     },
-    'fontSize': {
+    fontSize: {
       fieldType: 'dropdown',
       label: 'Font Size',
       additionalClass: 'inline',
@@ -190,7 +190,7 @@ export const field_properties = {
         { value: "40", label: "40" }
       ],
     },
-    'colorPicker': {
+    colorPicker: {
       fieldType: 'colorPicker',
       type: 'box',
       label: 'Color',
@@ -200,7 +200,7 @@ export const field_properties = {
     'bold/italic/strikethrough': {
       label: 'Bold/Italic/Strikethrough',
     },
-    'alignment': {
+    alignment: {
       label: 'Alignment'
     },
   },
@@ -238,10 +238,119 @@ export const field_properties = {
       position: 'right'
     },
   },
-  jbt_image: {},
-  jbt_date: [],
-  jbt_address: [],
+  jbt_image: {
+    uploadFile: {},
+    size: {},
+  },
+  jbt_date: {
+    fontFamily: {
+      fieldType: 'dropdown',
+      label: 'Font Family',
+      additionalClass: 'inline',
+      options: [
+        { value: "roboto", label: "Roboto" },
+        { value: "mono-space", label: "Mono-Space" },
+        { value: "Helvetica", label: "Helvetica" },
+      ],
+    },
+    fontSize: {
+      fieldType: 'dropdown',
+      label: 'Font Size',
+      additionalClass: 'inline',
+      options: [
+        { value: "8", label: "8" },
+        { value: "10", label: "10" },
+        { value: "12", label: "12" },
+        { value: "14", label: "14" },
+        { value: "16", label: "16" },
+        { value: "18", label: "18" },
+        { value: "20", label: "20" },
+        { value: "22", label: "22" },
+        { value: "24", label: "24" },
+        { value: "26", label: "26" },
+        { value: "28", label: "28" },
+        { value: "30", label: "30" },
+        { value: "32", label: "32" },
+        { value: "34", label: "34" },
+        { value: "36", label: "36" },
+        { value: "38", label: "38" },
+        { value: "40", label: "40" }
+      ],
+    },
+    colorPicker: {
+      fieldType: 'colorPicker',
+      type: 'box',
+      label: 'Color',
+      additionalClass: 'inline',
+      position: 'right'
+    },
+    'bold/italic/strikethrough': {
+      label: 'Bold/Italic/Strikethrough',
+    },
+    alignment: {
+      label: 'Alignment'
+    },
+  },
+  jbt_address: {
+    fontFamily: {
+      fieldType: 'dropdown',
+      label: 'Font Family',
+      additionalClass: 'inline',
+      options: [
+        { value: "roboto", label: "Roboto" },
+        { value: "mono-space", label: "Mono-Space" },
+        { value: "Helvetica", label: "Helvetica" },
+      ],
+    },
+    fontSize: {
+      fieldType: 'dropdown',
+      label: 'Font Size',
+      additionalClass: 'inline',
+      options: [
+        { value: "8", label: "8" },
+        { value: "10", label: "10" },
+        { value: "12", label: "12" },
+        { value: "14", label: "14" },
+        { value: "16", label: "16" },
+        { value: "18", label: "18" },
+        { value: "20", label: "20" },
+        { value: "22", label: "22" },
+        { value: "24", label: "24" },
+        { value: "26", label: "26" },
+        { value: "28", label: "28" },
+        { value: "30", label: "30" },
+        { value: "32", label: "32" },
+        { value: "34", label: "34" },
+        { value: "36", label: "36" },
+        { value: "38", label: "38" },
+        { value: "40", label: "40" }
+      ],
+    },
+    colorPicker: {
+      fieldType: 'colorPicker',
+      type: 'box',
+      label: 'Color',
+      additionalClass: 'inline',
+      position: 'right'
+    },
+    'bold/italic/strikethrough': {
+      label: 'Bold/Italic/Strikethrough',
+    },
+    alignment: {
+      label: 'Alignment'
+    },
+  },
   jbt_shape: [],
-  jbt_layout: []
+  jbt_layout: {
+    colorPicker: {
+      fieldType: 'colorPicker',
+      type: 'box',
+      label: 'Background Color',
+      additionalClass: 'inline',
+      position: 'right'
+    },
+    uploadFile: {},
+    size: {},
+  }
 };
 
